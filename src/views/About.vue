fde4d6140c7b45ebef141d1fa9509f8bb7259aa2
klkl <kuanglong0312@sina.com>
2020-11-19 11:31:26 +0800

    feat: 使用默认浏览器打开链接

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="openUrl">打开浏览器</button>
   </div>
 </template>
+<script>
+const { shell } = require('electron')
+export default {
+  name: 'About',
+  methods: {
+    openUrl () {
+      shell.openExternal('https://www.bing.com')
+    }
+  }
+}
+</script>
